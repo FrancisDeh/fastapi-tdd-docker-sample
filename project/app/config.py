b4a8a41f60a7e80107a17972b79fc9790d710ab7
Francis Deh <dfdemajesty699@gmail.com>
2023-06-28 22:50:11 +0200

    chore: added docker and docker compose

--- project/app/config.py
+++ project/app/config.py
@@ -12,6 +12,6 @@ class Settings(BaseSettings):
 
 
 @lru_cache()
-async def get_settings() -> BaseSettings:
+def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
